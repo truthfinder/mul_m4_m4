77a69fb2177da4c53f65eae30badbd20b4e2e83b
truthfinder <truthfinder@mail.ru>
2021-07-08 19:44:04 +0700

    CMake

---
+++ build.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+taskkill /IM PerfWatson2.exe /FI "STATUS eq RUNNING" /F
+
+rd /S /Q _build
+mkdir _build && cd _build
+
+rem mkdir build64 & pushd build64
+
+rem "Visual Studio 15 2017 Wun64"
+rem -A x64|Win32 -B "build32|build64" -S \path_to_source\
+rem --build build64 --config Release
+cmake .. -G "Visual Studio 16 2019" -A x64
+cmake --build . --config Release --clean-first
+
+rem popd
